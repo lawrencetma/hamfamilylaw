bbae6bee55377fb3e3228372a4dbb7156b4da287
Lawrence Ma <lawrencema@Lawrences-MacBook-Air.local>
2018-06-11 00:54:17 -0700

    Adds initial pages and background image

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'hamfamilylaw#home'
+
+  get '/home' => 'hamfamilylaw#home'
+  get '/about' => 'hamfamilylaw#about'
+  get '/practice' => 'hamfamilylaw#practice'
+  get '/testimonials' => 'hamfamilylaw#testimonials'
+  get '/contact' => 'hamfamilylaw#contact'
 end
